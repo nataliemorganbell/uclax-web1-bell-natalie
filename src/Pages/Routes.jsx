aef1c96cb5c19a0869bccedb96b399d7c1bddaa1
Natalie Bell <nataliemorgan.bell@gmail.com>
2024-07-29 19:02:30 -0500

    L6: Complete

--- src/Pages/Routes.jsx
+++ src/Pages/Routes.jsx
@@ -11,7 +11,7 @@ import PagesLayout from "@/Common/PagesLayout/PagesLayout.jsx";
 
 // page components
 import Home from "./Home";
-import Staff from "./Staff";
+import Staff from "./Staff/Staff.jsx";
 import Contact from "./Contact";
 
 // course work page components
